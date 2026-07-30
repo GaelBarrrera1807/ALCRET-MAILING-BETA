15753ca1092c328eac7b49a4d9e3f5cd9e3e7bf0
Gael Barrera Macias <223110373@utvam.edu.mx>
2026-07-30 19:59:05 +0200

    fix(deps): add django_celery_beat to INSTALLED_APPS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -62,6 +62,7 @@ DJANGO_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_beat',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
